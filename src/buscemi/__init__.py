bb052baf13ece2e6e86fe836c73762129f01cedd
Joe Cross <joe.mcross@gmail.com>
2019-02-13 03:18:07 -0800

    Make Connection.stop protected

--- src/buscemi/__init__.py
+++ src/buscemi/__init__.py
@@ -1,27 +1,33 @@
 import asyncio
+import copy
+import functools
+import secrets
 
 from . import parsing
-from .connection import _UciConnection, _read
+from .connection import UciConnection, read
 from .models import Position, Search, SearchConfig
-
-
-
 
 
 __all__ = ["Connection", "Position", "Search", "SearchConfig", "open_uci_connection"]
 
 
+async def _async_noop():
+    pass
+
+
 # noinspection PyProtectedMember
 class Connection:
-    def __init__(self, conn: _UciConnection):
+    def __init__(self, conn: UciConnection):
         self._conn = conn
         self._initialized = False
         self._options = {}
+        self._set_options = {}
 
         self._search: Search = None
 
-    async def __aenter__(self):
+    async def __aenter__(self) -> "Connection":
         await self.initialize()
+        return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         await self.close()
@@ -42,68 +48,82 @@ class Connection:
         self._initialized = True
 
     async def set_options(self, **options) -> None:
+        await self._stop()
         for name, value in options.items():
             opt = self._options[name.lower()]
             name, value = parsing.format_set_option(opt, value)
             await self._conn.setoption(name, value)
+        self._set_options.update(options)
 
-    async def search(self, *, pos: Position = None, config: SearchConfig = None, engine_config: dict = None) -> Search:
-        pos = pos or Position()
+    async def search(self, *, position: Position = None, config: SearchConfig = None) -> Search:
+        position = position or Position()
         config = config or SearchConfig()
 
-        await self.stop()
-        await self.set_options(**engine_config)
+        await self._stop()
         await self._conn.ucinewgame()
-        await self._conn.position(fen=pos.fen, moves=pos.moves)
+        await self._conn.position(fen=position.fen, moves=position.moves)
         await self._conn.isready()
         await self._conn.go(config.render())
 
         assert self._search is None
+        search_id = secrets.token_hex(16)
         search = self._search = Search(
-            pos=pos,
+            position=position,
             config=config,
-            engine_config=engine_config,
+            engine_config=copy.deepcopy(self._set_options),
 
+            _id=search_id,
             _done=asyncio.Event(),
             _progress=asyncio.Event(),
-            _stop_func=self.stop
+            _stop_func=functools.partial(self._stop, search_id)
         )
         asyncio.create_task(self._run_search(search))
         return self._search
 
-    async def stop(self) -> None:
+    async def _run_search(self, search: Search) -> None:
+        def progress():
+            search._progress.set()
+            search._progress.clear()
+
+        async for line in read(self._conn, timeout=0.5):
+            if line.startswith("info"):
+                search.info.append(line)
+                progress()
+            elif line.startswith("bestmove"):
+                search.bestmove = line
+                progress()
+                break
+
+        self._search._done.set()
+
+    async def _stop(self, search_id: str = None) -> None:
         await self._conn.stop()
+
+        # nothing running
         if not self._search:
             return
+
+        # allow None so this class can call _stop() without knowing the current search
+        # require search id from Search instances so we don't have a dangling _stop_func
+        if search_id and search_id != self._search._id:
+            return
+
+        # disconnect Search._stop_func and drain pending info
+        self._search._stop_func = _async_noop
         await self._search._done.wait()
+
         self._search = None
 
     async def close(self) -> None:
         if not self._conn:
             return
-        await self.stop()
+        await self._stop()
         await self._conn.quit()
         self._conn = None
 
-    async def _run_search(self, search: Search) -> None:
-        def toggle_status():
-            search._progress.set()
-            search._progress.clear()
-
-        async for line in _read(self._conn, timeout=0.5):
-            if line.startswith("info"):
-                search.info.append(line)
-                toggle_status()
-            elif line.startswith("bestmove"):
-                search.bestmove = line
-                toggle_status()
-                break
-
-        self._search._done.set()
-
 
 async def open_uci_connection(executable: str) -> Connection:
-    conn = await _UciConnection.from_executable(executable)
+    conn = await UciConnection.from_executable(executable)
     engine = Connection(conn)
     await engine.initialize()
     return engine
--- src/buscemi/connection.py
+++ src/buscemi/connection.py
@@ -5,12 +5,12 @@ import re
 from typing import AsyncIterator, Iterable, List, Optional, Pattern, Union
 
 
-__all__ = ["_UciConnection", "_read", "_read_until", "_write"]
+__all__ = ["UciConnection", "read", "read_until", "write"]
 
 logger = logging.getLogger(__name__)
 
 
-class _UciConnection:
+class UciConnection:
     """
     No input or state validation.  Keep track of what your engine is doing somewhere else.
 
@@ -24,7 +24,7 @@ class _UciConnection:
         self.proc = proc
 
     @classmethod
-    async def from_executable(cls, executable: str) -> "_UciConnection":
+    async def from_executable(cls, executable: str) -> "UciConnection":
         proc = await asyncio.create_subprocess_exec(
             executable,
             stdin=asyncio.subprocess.PIPE,
@@ -33,27 +33,27 @@ class _UciConnection:
         return cls(proc)
 
     async def uci(self) -> List[str]:
-        await _write(self, "uci")
-        return [x async for x in _read_until(self, "uciok")]
+        await write(self, "uci")
+        return [x async for x in read_until(self, "uciok")]
 
     async def debug(self, enable: bool = True) -> None:
         mode = "on" if enable else "off"
-        await _write(self, f"debug {mode}")
+        await write(self, f"debug {mode}")
 
     async def isready(self) -> List[str]:
-        await _write(self, "isready")
-        return [x async for x in _read_until(self, "readyok")]
+        await write(self, "isready")
+        return [x async for x in read_until(self, "readyok")]
 
     async def setoption(self, name: str, value: Optional[str] = None) -> None:
         cmd = f"setoption name {name}"
         if value is not None:
             cmd += f" value {value}"
-        await _write(self, cmd)
+        await write(self, cmd)
 
     # NOT SUPPORTED: register
 
     async def ucinewgame(self) -> None:
-        await _write(self, "ucinewgame")
+        await write(self, "ucinewgame")
 
     async def position(self, fen: Optional[str] = None, moves: Optional[Iterable[str]] = None) -> None:
         if not (bool(fen) ^ bool(moves)):
@@ -62,26 +62,26 @@ class _UciConnection:
             cmd = f"position fen {fen}"
         else:
             cmd = f"position startpos moves " + " ".join(moves)
-        await _write(self, cmd)
+        await write(self, cmd)
 
     async def go(self, args: List[str]) -> None:
-        await _write(self, f"go {' '.join(args)}")
+        await write(self, f"go {' '.join(args)}")
 
     async def stop(self) -> None:
-        await _write(self, "stop")
+        await write(self, "stop")
 
     async def ponderhit(self) -> None:
-        await _write(self, "ponderhit")
+        await write(self, "ponderhit")
 
     async def quit(self) -> None:
         if not self.proc:
             return
-        await _write(self, "quit")
+        await write(self, "quit")
         await self.proc.wait()
         self.proc = None
 
 
-async def _write(conn: _UciConnection, data: str, wait: bool = True) -> None:
+async def write(conn: UciConnection, data: str, wait: bool = True) -> None:
     if not data.endswith("\n"):
         data += "\n"
     conn.proc.stdin.write(data.encode("ascii"))
@@ -90,7 +90,7 @@ async def _write(conn: _UciConnection, data: str, wait: bool = True) -> None:
     logger.debug(f">>> {data!r}")
 
 
-async def _read(conn: _UciConnection, *, timeout: float = None) -> AsyncIterator[str]:
+async def read(conn: UciConnection, *, timeout: float = None) -> AsyncIterator[str]:
     readline = conn.proc.stdout.readline
     while True:
         try:
@@ -103,10 +103,10 @@ async def _read(conn: _UciConnection, *, timeout: float = None) -> AsyncIterator
             yield line.strip()
 
 
-async def _read_until(conn: _UciConnection, pattern: Union[str, Pattern], *, timeout=None) -> AsyncIterator[str]:
+async def read_until(conn: UciConnection, pattern: Union[str, Pattern], *, timeout=None) -> AsyncIterator[str]:
     if isinstance(pattern, str):
         pattern = re.compile(pattern)
-    async for line in _read(conn, timeout=timeout):
+    async for line in read(conn, timeout=timeout):
         yield line
         if pattern.match(line):
             break
